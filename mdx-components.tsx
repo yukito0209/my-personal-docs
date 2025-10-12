0e8ef65448494a73cb9e7ba96190646d9747cdc3
Kerwin Wang <76610895+yukito0209@users.noreply.github.com>
2025-10-12 21:05:18 +0800

    feat: 在文档页面添加了一篇文档

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,16 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import type { MDXComponents } from "mdx/types";
+import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
+import { Step, Steps } from "fumadocs-ui/components/steps";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Accordion,
+    Accordions,
+    Step,
+    Steps,
     ...components,
   };
 }
